c0fe2b7268f0bdc67896b857fd307539d5eb2e8c
Osama Jouda <oj.turk@gmail.com>
2022-10-12 18:32:12 +0300

    Adding controllers Adding password middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,8 @@ class Kernel extends HttpKernel
         // sanctum middleware
         'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
         'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
+
+        //application password
+        'password' => \App\Http\Middleware\CheckPassword::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware'=>['api','password']],static function(){
+
+    Route::group(['prefix'=>'admin'],static function(){
+        Route::post('register',[AdminController::class,'register']);
+    });
+
+    Route::group(['prefix'=>'user'],static function(){
+
+    });
+
+    Route::group(['prefix'=>'delivery'],static function(){
+
+    });
+
 });
+
+
