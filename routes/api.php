6bbfe12a64f5e15b9668b52c8d3f7481722c7a9a
Osama Jouda <oj.turk@gmail.com>
2022-10-13 10:17:11 +0300

    Adding admin change password function and route

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ Route::group(['middleware'=>['api','password']],static function(){
         Route::post('register',[AdminController::class,'register']);
         Route::post('login',[AdminController::class,'login']);
         Route::group(['middleware'=>['auth:sanctum','abilities:admin']],static function(){
+            Route::post('update',[AdminController::class,'update']);
+            Route::post('change_password',[AdminController::class,'changePassword']);
             Route::post('logout',[AdminController::class,'logout']);
             Route::post('info',[AdminController::class,'info']);
         });
